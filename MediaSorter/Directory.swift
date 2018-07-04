cbdb4265b56fb8e93d323323cf379f50e69a95fb
dougdiego <doug@diego.org>
2018-07-04 15:51:07 -0700

    Adding support for more image types and an app icon

--- MediaSorter/Directory.swift
+++ MediaSorter/Directory.swift
@@ -12,7 +12,7 @@ import AppKit
 import AVFoundation
 
 public struct Metadata: CustomDebugStringConvertible, Equatable {
-
+    
     let name: String
     let createDate: Date
     let modifiedDate: Date
@@ -23,7 +23,7 @@ public struct Metadata: CustomDebugStringConvertible, Equatable {
     let url: URL
     var newPath: String
     var exifDateTimeOriginal: Date?
-
+    
     init(fileURL: URL,
          name: String,
          createDate: Date,
@@ -32,7 +32,7 @@ public struct Metadata: CustomDebugStringConvertible, Equatable {
          icon: NSImage,
          isFolder: Bool,
          color: NSColor ) {
-
+        
         self.name = name
         self.createDate = createDate
         self.modifiedDate = modifiedDate
@@ -43,21 +43,21 @@ public struct Metadata: CustomDebugStringConvertible, Equatable {
         self.url = fileURL
         self.newPath = ""
     }
-
+    
     public var debugDescription: String {
         return name + " " + "Folder: \(isFolder)" + " Size: \(size)"
     }
-
+    
     public var fileExtension: String? {
         let components = name.components(separatedBy: ".")
-
+        
         guard components.count > 1 else {
             return nil
         }
-
+        
         return components.last
     }
-
+    
 }
 
 // MARK: - Metadata  Equatable
@@ -73,10 +73,10 @@ public enum OutputDirs: String {
 }
 
 public class Directory {
-
+    
     fileprivate var files: [Metadata] = []
     let url: URL
-
+    
     public enum FileOrder: String {
         case name
         case newPath
@@ -85,11 +85,11 @@ public class Directory {
         case modifiedDate
         case size
     }
-
+    
     public init(folderURL: URL) {
         url = folderURL
     }
-
+    
     public func loadFiles(currentFile: @escaping (String) -> Void,
                           completion: @escaping () -> Void,
                           failure: @escaping (_ error: Error?) -> Void ) {
@@ -106,10 +106,10 @@ public class Directory {
                                                                includingPropertiesForKeys: requiredAttributes,
                                                                options: [.skipsHiddenFiles, .skipsPackageDescendants, .skipsSubdirectoryDescendants],
                                                                errorHandler: nil) {
-
+                
                 while let url = enumerator.nextObject() as? URL {
                     log.debug( "\(url )")
-
+                    
                     do {
                         let properties = try  (url as NSURL).resourceValues(forKeys: requiredAttributes)
                         var icon = properties[URLResourceKey.effectiveIconKey] as? NSImage  ?? NSImage()
@@ -139,20 +139,20 @@ public class Directory {
                     }
                 }
             }
-
+            
             completion()
         }
-
+        
     }
-
+    
     private func thumbnailForVideoAtURL(url: URL) -> NSImage? {
-
+        
         let asset = AVAsset(url: url)
         let assetImageGenerator = AVAssetImageGenerator(asset: asset)
-
+        
         var time = asset.duration
         time.value = min(time.value, 2)
-
+        
         do {
             let imageRef = try assetImageGenerator.copyCGImage(at: time, actualTime: nil)
             //return NSImage(CGImage: imageRef)
@@ -162,7 +162,7 @@ public class Directory {
             return nil
         }
     }
-
+    
     func contentsOrderedBy(_ orderedBy: FileOrder, ascending: Bool) -> [Metadata] {
         let sortedFiles: [Metadata]
         switch orderedBy {
@@ -199,85 +199,113 @@ public class Directory {
         }
         return sortedFiles
     }
-
+    
     public func processNewPath(_ photoIdentifer: String = DefaultValues.identifer.rawValue,
                                dateFormat: String? = DefaultValues.dateFormat.rawValue,
                                currentFile: @escaping (String) -> Void,
                                completion: @escaping () -> Void,
                                failure: @escaping (_ error: Error?) -> Void ) {
-
+        
         DispatchQueue.global(qos: .userInitiated).async {
-
+            
             let fileManager = FileManager.default
-
+            
             // TODO: don't hardcode this
             var destFolderPath = ""
             let videoDestFolder = "video"
             let imageDestFolder = "image"
             let errorDestFolder = "error"
-
+            
             for (index, file) in self.files.enumerated() {
                 var processedFile = file
                 log.debug("file: \(file) ext: \(String(describing: file.fileExtension))")
-
+                
                 log.debug("Processing name: \(file.name) path: \(file.url)")
                 currentFile(file.name)
                 //sleep(1)
                 var date: Date?
                 var newPath: String?
-
+                
                 // TODO: don't hardcode file extensions
                 if let fileExtension = file.fileExtension {
                     switch fileExtension.uppercased() {
-                    case "JPG", "PNG", "HEIC", "CR2":
+                    case "JPG", "PNG", "HEIC", "CR2", "JPEG", "TIF":
                         date = MediaUtil.getPhotoExifDateTimeOriginal(file.url.path)
                         destFolderPath = imageDestFolder
                     case "MOV", "MP4", "M4V":
                         date = MediaUtil.getVideoCreationDate(URL(fileURLWithPath: file.url.path))
                         destFolderPath = videoDestFolder
-
+                        
                         // Live Photo should go in image directory
                         let jpgLivePhotoPath = file.url.path.replacingOccurrences(of: fileExtension, with: "JPG")
                         let heicLivePhotoPath = file.url.path.replacingOccurrences(of: fileExtension, with: "HEIC")
-                        log.debug("jpgLivePhotoPath: \(jpgLivePhotoPath)")
-                        log.debug("heicLivePhotoPath: \(heicLivePhotoPath)")
-
-                        if fileManager.fileExists(atPath: jpgLivePhotoPath) ||
-                            fileManager.fileExists(atPath: heicLivePhotoPath) {
-                            log.debug("JPG/HEIC Live Photo Found")
+                        let jpegLivePhotoPath = file.url.path.replacingOccurrences(of: fileExtension, with: "JPEG")
+                        let tifLivePhotoPath = file.url.path.replacingOccurrences(of: fileExtension, with: "TIF")
+//                        log.debug("jpgLivePhotoPath: \(jpgLivePhotoPath)")
+//                        log.debug("heicLivePhotoPath: \(heicLivePhotoPath)")
+                        
+//                        if fileManager.fileExists(atPath: jpgLivePhotoPath) ||
+//                            fileManager.fileExists(atPath: heicLivePhotoPath) ||
+//                            fileManager.fileExists(atPath: jpegLivePhotoPath) ||
+//                            fileManager.fileExists(atPath: tifLivePhotoPath) {
+//                            log.debug("JPG/HEIC/JPEG/TIF Live Photo Found")
+//                            destFolderPath = imageDestFolder
+//                        }
+                        if fileManager.fileExists(atPath: jpgLivePhotoPath) {
+                            log.debug("JPG Live Photo Found")
+                            let matchingPhoto = file.url.path.replacingOccurrences(of: fileExtension, with: "JPG")
+                            date = MediaUtil.getPhotoExifDateTimeOriginal(matchingPhoto)
                             destFolderPath = imageDestFolder
                         }
-
+                        if fileManager.fileExists(atPath: heicLivePhotoPath) {
+                            log.debug("HEIC Live Photo Found")
+                            let matchingPhoto = file.url.path.replacingOccurrences(of: fileExtension, with: "HEIC")
+                            date = MediaUtil.getPhotoExifDateTimeOriginal(matchingPhoto)
+                            destFolderPath = imageDestFolder
+                        }
+                        if fileManager.fileExists(atPath: jpegLivePhotoPath) {
+                            log.debug("JPEG Live Photo Found")
+                            let matchingPhoto = file.url.path.replacingOccurrences(of: fileExtension, with: "JPEG")
+                            date = MediaUtil.getPhotoExifDateTimeOriginal(matchingPhoto)
+                            destFolderPath = imageDestFolder
+                        }
+                        if fileManager.fileExists(atPath: tifLivePhotoPath) {
+                            log.debug("TIF Live Photo Found")
+                            let matchingPhoto = file.url.path.replacingOccurrences(of: fileExtension, with: "TIF")
+                            date = MediaUtil.getPhotoExifDateTimeOriginal(matchingPhoto)
+                            destFolderPath = imageDestFolder
+                        }
+                        
                     default:
                         log.debug("No valid extension found for: \(fileExtension.uppercased())")
                     }
                 }
                 log.debug("destFolderPath: \(destFolderPath)")
                 log.debug("date: \(String(describing: date))")
-
+                
                 if let date = date {
                     processedFile.exifDateTimeOriginal = date
                     let dateFormatter = DateFormatter()
                     dateFormatter.dateFormat = dateFormat
                     let fDate = dateFormatter.string(from: date)
                     let filename: String?
-
+                    
                     if photoIdentifer.isEmpty {
                         filename  = "\(fDate)-\(file.name)"
                     } else {
                         filename  = "\(fDate)-\(photoIdentifer)-\(file.name)"
                     }
-
+                    
                     log.debug("filename: \(String(describing: filename))")
                     if let filename = filename {
                         newPath = "\(destFolderPath)/\(filename)"
                     }
                 }
-
+                
                 if newPath == nil {
                     newPath = "\(errorDestFolder)/\(file.name)"
                 }
-
+                
                 processedFile.newPath = newPath!
                 self.files[index] = processedFile
             }
@@ -285,7 +313,7 @@ public class Directory {
             completion()
         }
     }
-
+    
 }
 
 // MARK: - Sorting
